c3cc7701e14220d453b5601fb990e6701848dea1
shock <shock_cq@163.com>
2018-11-01 09:43:37 +0800

    init env setting e.g. PORT=90000

--- pos.go
+++ pos.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/lexkong/log"
+	"os"
+)
+
 func main()  {
-	
+	err :=godotenv.Load()
+	if err != nil {
+		log.Fatal("Load the env error: ",err)
+	}
+	fmt.Println(os.Getenv("PORT"));
 }
